e2add94ea7a100c2f14d56da6babafdab1c91c01
Tim Poštuvan <tim.postuvan@gmail.com>
2023-05-15 22:40:29 +0200

    Improve code style

--- src/exploration/import_graph.py
+++ src/exploration/import_graph.py
@@ -8,11 +8,8 @@ import os
 
 class AcademicGraph:
     def __init__(self):
-
         df = pd.read_csv(edge_list)
         df["weight"] = df["Total"]
-        self.df = df
-
 
         # connect to locations
         country_pairings = pd.read_csv(os.path.join(data_dir, "university_country_pairing.csv"))
@@ -22,31 +19,30 @@ class AcademicGraph:
         self.us_unis = us_unis
         self.non_us_unis = non_us_unis 
 
-        self.df = self.df.merge(country_pairings[["DegreeInstitutionId", "CountryName"]], on = "DegreeInstitutionId", how = "left")
-        
-        self.df["location"] = self.df["CountryName"].apply(lambda x: 1 if x=="United States" else 0)  # add a 1 for US locations, used later.
+        self.df = df.merge(country_pairings[["DegreeInstitutionId", "CountryName"]], on="DegreeInstitutionId", how="left")
+        self.df["location"] = self.df["CountryName"].apply(lambda x: 1 if x == "United States" else 0)  # add a 1 for US locations, used later.
 
     def full_graph(self) -> nx.Graph:
-        G = nx.from_pandas_edgelist(self.df, "DegreeInstitutionId", "InstitutionId", ["TaxonomyLevel", "TaxonomyValue", "InstitutionName", "DegreeInstitutionName", "weight", "Men", "Women"])
-
+        G = nx.from_pandas_edgelist(
+            self.df, 
+            source="DegreeInstitutionId",
+            targer="InstitutionId",
+            edge_attr=["TaxonomyLevel", "TaxonomyValue", "InstitutionName", "DegreeInstitutionName", "weight", "Men", "Women"]
+        )
         return G
-    
-    def segemented_graphs(self, limit_to_US = True): 
-        temp = self.df
 
+    def segmented_graphs(self, limit_to_US: bool = True): 
+        temp = self.df.copy()
         temp = temp[temp["TaxonomyLevel"] == "Domain"]
-
         groups = temp.groupby("TaxonomyValue")          # groupby the domains
 
-        graph_list = []
-        features = []
-        graph_labels= []
+        graph_list, features, graph_labels = [], [], []
 
         for name, group in groups:
             print(f"Preparing domain: {name}")
             feat_dict = {}
 
-            # all hires, men, women, mean from US
+            # all hires, men, women, percentage from US
             total, men, women, location = group["Total"].sum(), group["Men"].sum(), group["Women"].sum(), group["location"].mean()         
 
             features.append({"Total": total, "Men": men, "Women": women, "Prop_US": location})
